39e4643c332fde4d390903016e395b00568232fa
Nathan Viniconis <revrick@gmail.com>
2016-12-16 13:24:24 -0500

    Random suggestions
    - initialize everything possible out of module.exports.loop. No reason to do this every tick.
    - condense role's into a map

--- main.js
+++ main.js
@@ -3,67 +3,44 @@ require('prototype.creep')();
 
 var manage = require('manage');
 
-var roleBuilder = require('role.builder');
-var roleGatherer = require('role.gatherer');
-var roleMiner = require('role.miner');
-var roleUpgrader = require('role.upgrader');
-var roleRepairer = require('role.repairer');
-var roleLongRangeHarvester = require('role.longRangeHarvester');
-var roleCrossRoomBuilder = require('role.crossRoomBuilder');
-var roleGuard = require('role.guard');
-var roleRangedGuard = require('role.rangedGuard');
+var roleMap = {};
+roleMap['builder'] = require('role.builder');
+roleMap['gatherer'] = require('role.gatherer');
+roleMap['miner'] = require('role.miner');
+roleMap['upgrader'] = require('role.upgrader');
+roleMap['repairer'] = require('role.repairer');
+roleMap['lrHarvester'] = require('role.longRangeHarvester');
+roleMap['crBuilder'] = require('role.crossRoomBuilder');
+roleMap['guard'] = require('role.guard');
+roleMap['rangedGuard'] = require('role.rangedGuard');
+
 var towerControl = require('tower');
+
+var BUILDER_LIMIT = 2;
+var GATHERER_LIMIT = 2;
+var MINER_LIMIT = 2;
+var UPGRADER_LIMIT = 2;
+var REPAIRER_LIMIT = 2;
+var LONG_RANGE_HARVESTER_LIMIT = 5;
+var CROSS_ROOM_BUILDER_LIMIT = 1;
+var GUARD_LIMIT = 2;
+var RANGED_GUARD_LIMIT = 1;
 
 module.exports.loop = function () {
     
     // console.log(Game.time);
     
-    var BUILDER_LIMIT = 2;
-    var GATHERER_LIMIT = 2;
-    var MINER_LIMIT = 2;
-    var UPGRADER_LIMIT = 2;
-	var REPAIRER_LIMIT = 2;
-	var LONG_RANGE_HARVESTER_LIMIT = 5;
-	var CROSS_ROOM_BUILDER_LIMIT = 1;
-	var GUARD_LIMIT = 2;
-	var RANGED_GUARD_LIMIT = 1;
-    
     manage.run(BUILDER_LIMIT,GATHERER_LIMIT,MINER_LIMIT,UPGRADER_LIMIT,REPAIRER_LIMIT,LONG_RANGE_HARVESTER_LIMIT,CROSS_ROOM_BUILDER_LIMIT,GUARD_LIMIT,RANGED_GUARD_LIMIT);
     
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if (creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
+        if (roleMap[creep.memory.role] != undefined) {
+            roleMap[creep.memory.role].run(creep);
         }
-        if (creep.memory.role == 'gatherer') {
-            roleGatherer.run(creep);
-        }
-        if (creep.memory.role == 'miner') {
-            roleMiner.run(creep);
-        }
-        if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-		if (creep.memory.role == 'repairer') {
-			roleRepairer.run(creep);
-		}
-		if (creep.memory.role == 'lrHarvester') {
-			roleLongRangeHarvester.run(creep);
-		}
-		if (creep.memory.role == 'crBuilder') {
-			roleCrossRoomBuilder.run(creep);
-		}
-		if (creep.memory.role == 'guard') {
-			roleGuard.run(creep);
-		}
-		if (creep.memory.role == 'rangedGuard') {
-			roleRangedGuard.run(creep);
-		}
     }
 	
-	var towers = Game.spawns['Spawner_Main'].room.find(FIND_STRUCTURES, {
-		filter: (structure) => structure.structureType == STRUCTURE_TOWER
-	});
-	towers.forEach((tower) => towerControl.run(tower));
-    
+    var towers = Game.spawns['Spawner_Main'].room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType == STRUCTURE_TOWER
+    });
+    towers.forEach((tower) => towerControl.run(tower));
 }
